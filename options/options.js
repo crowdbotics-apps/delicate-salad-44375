d2fdb01efd19e8b574248376cd9b17990941636f
Crowdbotics <team@crowdbotics.com>
2023-11-06 19:53:05 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "delicate-salad-44375",
+  url: "https://delicate-salad-44375.botics.co",
+  api: "https://delicate-salad-44375.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
