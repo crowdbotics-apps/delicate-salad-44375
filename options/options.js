fa49db85177566b9e82fc5fd1136b8dad187e067
Kanyinsola Saheed <kanyinsola.saheed.ec@crowdbotics.com>
2023-11-21 16:41:23 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "delicate-salad-44375",
   url: "https://delicate-salad-44375.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
