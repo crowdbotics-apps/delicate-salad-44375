1ec66d5b78b5c5e641f5a582523d26bb49b32a05
Kanyinsola Saheed <kanyinsola.saheed.ec@crowdbotics.com>
2023-11-21 16:43:56 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/TermsAndConditionsScreen/index.js
+++ screens/TermsAndConditionsScreen/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { SafeAreaView, ScrollView, StyleSheet, Text, Image, View } from 'react-native';
+import { SafeAreaView, ScrollView, Text, StyleSheet, Image } from 'react-native';
 
-const PrivacyPolicyScreen = () => {
+const TermsAndConditionsScreen = () => {
   return <SafeAreaView style={styles.container}>
-      <ScrollView style={styles.scrollView}>
-        <View style={styles.header}>
-          <Image style={styles.logo} source={{
-          uri: 'https://tinyurl.com/42evm3m3'
-        }} />
-          <Text style={styles.title}>Privacy Policy</Text>
-        </View>
+      <ScrollView contentContainerStyle={styles.contentContainer}>
+        <Image style={styles.logo} source={{
+        uri: 'https://tinyurl.com/42evm3m3'
+      }} />
+        <Text style={styles.title}>Terms and Conditions</Text>
         <Text style={styles.text}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </Text>
@@ -20,30 +18,28 @@ const PrivacyPolicyScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff'
+    backgroundColor: '#F5F5F5'
   },
-  scrollView: {
-    marginHorizontal: 20
-  },
-  header: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginVertical: 20
+  contentContainer: {
+    padding: 16
   },
   logo: {
-    width: 50,
-    height: 50,
-    marginRight: 10
+    width: 100,
+    height: 100,
+    alignSelf: 'center',
+    marginBottom: 16
   },
   title: {
     fontSize: 24,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    color: '#333',
+    textAlign: 'center',
+    marginBottom: 16
   },
   text: {
     fontSize: 16,
-    lineHeight: 24,
-    color: '#333'
+    color: '#333',
+    lineHeight: 24
   }
 });
-export default PrivacyPolicyScreen;
+export default TermsAndConditionsScreen;
